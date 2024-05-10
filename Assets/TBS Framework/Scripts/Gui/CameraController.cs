ba01d5bb4748209db3dd4324f8108c2800dc7c3e
Filipe Serrazina <fserrazina99@gmail.com>
2024-05-10 14:32:09 +0700

    Implemented Camera Bounds

--- Assets/TBS Framework/Scripts/Gui/CameraController.cs
+++ Assets/TBS Framework/Scripts/Gui/CameraController.cs
@@ -8,8 +8,12 @@ namespace TbsFramework.Gui
         public float ScrollEdge = 0.01f;
         public float SnapSpeed = 10f;
 
+        [SerializeField] private Vector2 horizontalBounds;
+        [SerializeField] private Vector2 verticalBounds;
+
         private bool isSnapping = false;
         private Vector3 targetPosition;
+
 
         private void Update()
         {
@@ -40,12 +44,49 @@ namespace TbsFramework.Gui
                     transform.Translate(transform.up * Time.deltaTime * -ScrollSpeed, Space.World);
                 }
             }
+
+            transform.position = CorrectVectorWithinBounds(transform.position, horizontalBounds, verticalBounds);
         }
 
         public void SnapToPosition(Vector3 position)
         {
             targetPosition = new Vector3(position.x, position.y, transform.position.z);
             isSnapping = true;
+
+            targetPosition = CorrectVectorWithinBounds(targetPosition, horizontalBounds, verticalBounds);
+        }
+
+        private Vector3 CorrectVectorWithinBounds(Vector3 vector, Vector2 horizontalBounds, Vector2 verticalBounds)
+        {
+            Vector3 result = vector;
+
+            if (result.x < horizontalBounds.x)
+            {
+                Vector3 newPos = result;
+                newPos.x = horizontalBounds.x;
+                result = newPos;
+            }
+            else if (result.x > horizontalBounds.y)
+            {
+                Vector3 newPos = result;
+                newPos.x = horizontalBounds.y;
+                result = newPos;
+            }
+
+            if (result.y < verticalBounds.x)
+            {
+                Vector3 newPos = result;
+                newPos.y = verticalBounds.x;
+                result = newPos;
+            }
+            else if (result.y > verticalBounds.y)
+            {
+                Vector3 newPos = result;
+                newPos.y = verticalBounds.y;
+                result = newPos;
+            }
+
+            return result;
         }
     }
 }
